ebd24c2498e8d8ddf3bd1b6c563c784d0e070787
A__Djordjevich <acadjordjevic02@gmail.com>
2025-05-07 10:49:05 +0200

    Add files via upload

---
+++ events.php
@@ -0,0 +1,58 @@
+<?php
+$events = [
+    [
+        'img' => './content/events/1.jpg',
+        'link' => 'https://www.instagram.com/p/DHXytlro1S0/',
+        'title' => 'Hot Lap Challenge - Australija',
+        'date' => '19.03.2025.'
+    ],
+    [
+        'img' => './content/events/2.jpg',
+        'link' => 'https://www.instagram.com/p/DFpBKPoIrt7/',
+        'title' => 'Hot Lap Challenge - Italija',
+        'date' => '04.02.2025.'
+    ],
+    [
+        'img' => './content/events/3.jpg',
+        'link' => 'https://www.instagram.com/p/DEmA_soIPGB/',
+        'title' => 'Endurance Trka - Nürburgring',
+        'date' => '09.01.2025.'
+    ],
+    [
+        'img' => './content/events/4.jpg',
+        'link' => 'https://www.instagram.com/p/DDHVuEZsiMi/',
+        'title' => 'Hot Lap Challenge - Holandija',
+        'date' => '03.12.2024.'
+    ],
+    [
+        'img' => './content/events/5.jpg',
+        'link' => 'https://www.instagram.com/p/DB-wOQhM5KD/',
+        'title' => 'Hot Lap Challenge - Španija',
+        'date' => '05.11.2024.'
+    ],
+    [
+        'img' => './content/events/6.jpg',
+        'link' => 'https://www.instagram.com/p/DA03oUJM_1G/',
+        'title' => 'Hot Lap Challenge - Japan',
+        'date' => '07.10.2024.'
+    ],
+    [
+        'img' => './content/events/7.jpg',
+        'link' => 'https://www.instagram.com/p/C_akQcuM1vz/',
+        'title' => 'Hot Lap Challenge - Austrija',
+        'date' => '02.09.2024.'
+    ],
+    [
+        'img' => './content/events/8.jpg',
+        'link' => 'https://www.instagram.com/p/C-HIQurox3-/',
+        'title' => 'Hot Lap Challenge - Brazil',
+        'date' => '01.08.2024.'
+    ],
+    [
+        'img' => './content/events/9.jpg',
+        'link' => 'https://www.instagram.com/p/C850namIwTE/',
+        'title' => 'Hot Lap Challenge - Belgija',
+        'date' => '02.07.2024.'
+    ],
+];
+?>
